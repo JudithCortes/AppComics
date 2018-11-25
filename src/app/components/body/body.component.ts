42f59beef44cc94188cc84d4aa04a92aa37126c8
Judith <baruki110214@gmail.com>
2018-11-24 22:33:15 -0600

    creando las primeras propiedades en la clase del componente

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,5 +6,8 @@ import{ Component } from '@angular/core';
     })
 
 export class BodyComponent{
-
+    frase: any = {
+        mensaje: 'Un gran poder requiere una gran responsabilidad',
+        autor: 'Ben Parker'
+    }
 }
