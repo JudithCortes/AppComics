5c326d5c9c417eae31399e931275e99f6976b9f8
Judith <baruki110214@gmail.com>
2018-11-24 22:59:52 -0600

    usando *ngIf y *ngFor

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,11 +6,13 @@ import{ Component } from '@angular/core';
     })
 
 export class BodyComponent{
-    
+
     mostrar=true;
 
     frase: any = {
         mensaje: 'Un gran poder requiere una gran responsabilidad',
         autor: 'Ben Parker'
-    }
+    };
+
+    personajes: string[] = ['Spiderman', 'Venom', 'Dr. Octopus'];
 }
