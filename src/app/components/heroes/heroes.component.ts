ada44528f024cf7aba69b8d95bf6f3c49fa371ce
Judith <baruki110214@gmail.com>
2018-11-28 17:21:01 -0600

    creando vista de heroe

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HeroesService, Heroe} from '../../servicios/heroes.service';
 //mport { from } from 'rxjs';
+//import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +11,20 @@ export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
 
-  constructor(private _heroesService:HeroesService ) { 
-    console.log("constructor");
+  constructor(private _heroesService:HeroesService
+    ) { 
+    //console.log("constructor");
   }
 
   ngOnInit() {
 
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
+    //console.log(this.heroes);
   }
+
+  //verHeroe(idx:number){
+    //this.router.navigate(['/heroe,idx'])
+  //}
 
 }
 
